e5f6be1cfabd21d535183a1a134ccbf5dc54143a
dark_dip2 <diptya99@gmail.com>
2023-07-24 21:03:46 +0530

    Mongo

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/test';
+const CONNECTION_URL = 'mongodb+srv://diptya99:Password67@cluster0.z4i3cww.mongodb.net/SocialMedia';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
